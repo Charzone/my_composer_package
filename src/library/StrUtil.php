995441cf2e57c758e2607ee3d64dd69f7f8451f3
ccz <269363194@qq.com>
2023-04-23 17:12:54 +0800

    init

---
+++ src/library/StrUtil.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace Monkey\MyComposerPackage\library;
+
+/**
+* Created by IntelliJ IDEA.
+*
+* @Author : monkey
+* @create 2023/4/8 23:42
+*/
+class StrUtil
+{
+
+    public function strLen($str){
+        return strlen($str);
+    }
+
+}
